edd2f5aea18366c96069e67b51109bf7ab9fc17b
whale-z <102846153+whale-z@users.noreply.github.com>
2022-06-16 00:44:37 +0800

    Update generate_feature.sh

--- baselines/track5/scripts/generate_feature.sh
+++ baselines/track5/scripts/generate_feature.sh
@@ -1,4 +1,4 @@
 python generate_feature.py --test_path ./data_qe/test.phase1.fluency.json \
 --out_path ./data_feature/output_phase1_fluency.json \
---bert_pretrain bert-base-chinese \
+--model_name_or_path bert-base-chinese \
 --checkpoint ./save_model/model.best.pt
